49810b5ccf8dda8a8302d7dfa5437c559c51ad1a
kabradshaw1 <itllbok82@gmail.com>
2023-01-31 09:33:21 -0500

    set up process

--- server/server/settings.py
+++ server/server/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('USER_NAME'),
+        'PASSWORD': config('DB_PW'),
+        'HOST': config('HOST'),
+        'PORT': '5432'
     }
 }
 
